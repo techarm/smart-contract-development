f24a375c1c9d0cab89cc22ada7df95276970d4bd
techarm <techarmdev@gmail.com>
2023-03-06 23:14:02 +0900

    作成のみ変更できる制御を追加(modifier)

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -3,13 +3,27 @@ pragma solidity >= 0.4.0 <= 0.8;
 
 contract Greeter {
     string private _greeting = "Hello World!";
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
 
     function greet() external view returns(string memory) {
         return _greeting;
     }
 
-    function setGreeting(string calldata greeting) external {
+    function setGreeting(string calldata greeting) external onlyOwner {
         _greeting = greeting;
+    }
+
+    function owner() public view returns(address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");   
+        _;
     }
 }
 

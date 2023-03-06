9a0b89e2087e4916e08e4b4f0c3c5034e0e31411
techarm <techarmdev@gmail.com>
2023-03-06 23:44:01 +0900

    OpenZeppelinのライブラリを使う

--- greeter/contracts/Greeter.sol
+++ greeter/contracts/Greeter.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >= 0.4.0 <= 0.8;
+pragma solidity ^0.8.0;
 
-contract Greeter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
     string private _greeting = "Hello World!";
-    address private _owner;
-
-    constructor() {
-        _owner = msg.sender;
-    }
 
     function greet() external view returns(string memory) {
         return _greeting;
@@ -15,15 +12,6 @@ contract Greeter {
 
     function setGreeting(string calldata greeting) external onlyOwner {
         _greeting = greeting;
-    }
-
-    function owner() public view returns(address) {
-        return _owner;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Ownable: caller is not the owner");   
-        _;
     }
 }
 
